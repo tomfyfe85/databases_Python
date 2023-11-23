08e552f59028db6be75225ee93f55d141a06c311
Tom Fyfe <tomfyfe85@gmail.com>
2023-11-23 23:58:33 +0000

    recipe.py - Recipe#__eq__ passes #test_recipes_are_equal in test_recipe.py

--- Phase_2_Challenges/recipe_directory/tests/test_recipe.py
+++ Phase_2_Challenges/recipe_directory/tests/test_recipe.py
@@ -3,29 +3,37 @@ from lib.recipe import Recipe
 """
 Recipe constructs with an id, name, cooking_time and rating
 """
+
+
 def test_recipe_constructs():
-    recipe = Recipe(1, "Test name", "Test cooking_time", "Test rating")
+    recipe = Recipe(1, "Test name", 5, 5)
     assert recipe.id == 1
     assert recipe.name == "Test name"
-    assert recipe.cooking_time == "Test cooking_time"
-    assert recipe.rating_1_to_5 == "Test rating"
+    assert recipe.cooking_time == 5
+    assert recipe.rating_1_to_5 == 5
+
 
 """
 We can format recipes to strings nicely
 """
-def test_artists_format_nicely():
-    recipe = Recipe(1, "Test name", "Test cooking_time", "Test rating")
-    assert str(recipe) == "Recipe(1, Test name, Test cooking_time, Test rating)"
+
+
+def test_recipes_format_nicely():
+    recipe = Recipe(1, "Test name", 3, 2)
+    assert str(recipe) == "Recipe(1, Test name, 3, 2)"
     # Try commenting out the `__repr__` method in lib/artist.py
     # And see what happens when you run this test again.
 
-# """
-# We can compare two identical artists
-# And have them be equal
-# """
-# def test_artists_are_equal():
-#     artist1 = Artist(1, "Test Artist", "Test Genre")
-#     artist2 = Artist(1, "Test Artist", "Test Genre")
-#     assert artist1 == artist2
-#     # Try commenting out the `__eq__` method in lib/artist.py
-#     # And see what happens when you run this test again.
+
+"""
+We can compare two identical recipes
+And have them be equal
+"""
+
+
+def test_recipes_are_equal():
+    recipe1 = Recipe(1, "Test Recipe", 70, 4)
+    recipe2 = Recipe(1, "Test Recipe", 70, 4)
+    assert recipe1 == recipe2
+    # Try commenting out the `__eq__` method in lib/artist.py
+    # And see what happens when you run this test again.
