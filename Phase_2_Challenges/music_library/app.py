cb68931d21355b9f99c4cdf971323bec1ab78ee2
Tom Fyfe <tomfyfe85@gmail.com>
2023-11-25 19:40:18 +0000

    08_wrapping_in_application_class complete

--- Phase_2_Challenges/music_library/app.py
+++ Phase_2_Challenges/music_library/app.py
@@ -3,6 +3,9 @@ from lib.artist_repository import ArtistRepository
 from lib.album_repository import AlbumRepository
 
 
+# connection = DatabaseConnection()
+# connection.connect()
+# connection.seed("seeds/music_library.sql")
 class Application:
     def __init__(self):
         self._connection = DatabaseConnection()
@@ -10,16 +13,35 @@ class Application:
         self._connection.seed("seeds/music_library.sql")
 
     def run(self):
-        artist_repository = ArtistRepository(self._connection)
-        artists = artist_repository.all()
-
-        for artist in artists:
-            print(f"{artist.id}: {artist.name} ({artist.genre})")
+        print("Welcome to the music library manager! \n")
+        print(
+            "What would you like to do? \n1 - List all albums \n2 - List all artists \n"
+        )
+        library_choice = input("Enter your choice:")
+        if library_choice == "2":
+            artist_repository = ArtistRepository(self._connection)
+            artists = artist_repository.all()
+            print("\nHere is the list of artists:")
+            for artist in artists:
+                print(f"{artist.id}: {artist.name} ({artist.genre})")
+        elif library_choice == "1":
+            album_repository = AlbumRepository(self._connection)
+            albums = album_repository.all()
+            print("\nHere is the list of albums:")
+            for album in albums:
+                print(
+                    f"{album.id}: {album.title} ({album.release_year}) ({album.artist_id})"
+                )
 
 
 if __name__ == "__main__":
     app = Application()
     app.run()
+
+
+# # if __name__ == "__main__":
+#     app = Application()
+#     app.run()
 # Retrieve all artists
 # artist_repository = ArtistRepository(connection)
 # for artist in artist_repository.all():
