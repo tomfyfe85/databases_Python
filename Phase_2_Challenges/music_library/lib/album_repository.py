0936aa82eafc96e8559e3df6bac6e9db669bc3d1
Tom Fyfe <tomfyfe85@gmail.com>
2023-11-29 18:16:34 +0000

    album_repository.py - AlbumRepository#create passes #test_create_album in test_album_repository.py

--- Phase_2_Challenges/music_library/lib/album_repository.py
+++ Phase_2_Challenges/music_library/lib/album_repository.py
@@ -8,7 +8,7 @@ class AlbumRepository:
     # Selecting all records
     # No arguments
     def all(self):
-        result = self._connection.execute("SELECT * FROM albums")
+        result = self._connection.execute("SELECT * from albums ORDER BY id ASC")
 
         return [
             Album(row["id"], row["title"], row["release_year"], row["artist_id"])
@@ -19,3 +19,15 @@ class AlbumRepository:
         rows = self._connection.execute("SELECT * from albums WHERE id = %s", [album_id])
         row = rows[0]
         return Album(row["id"], row["title"], row["release_year"], row["artist_id"])
+
+    def create(self, album):
+        self._connection.execute(
+            "INSERT INTO albums (title, release_year, artist_id) VALUES (%s, %s, %s)",
+            [album.title, album.release_year, album.artist_id],
+        )
+        return None
+
+    # # Delete an user by their id
+    # def delete(self, post_id):
+    #     self._connection.execute("DELETE FROM posts WHERE id = %s", [post_id])
+    #     return None
--- Phase_2_Challenges/music_library/tests/test_album_repository.py
+++ Phase_2_Challenges/music_library/tests/test_album_repository.py
@@ -39,3 +39,54 @@ def test_finds_a_specific_album_by_id(db_connection):
 
     album = repository.find(2)
     assert album == Album(2, "Surfer Rosa", 1988, 1)
+
+"""
+
+"""
+
+def test_create_album(db_connection):
+    db_connection.seed("seeds/music_library.sql")
+    repository = AlbumRepository(db_connection)
+
+    album = Album(None, "test", 2000, 1)
+    assert repository.create(album) == None
+
+    result = repository.all()
+    assert result == [
+        Album(1, "Doolittle", 1989, 1),
+        Album(2, "Surfer Rosa", 1988, 1),
+        Album(3, "Waterloo", 1974, 2),
+        Album(4, "Super Trouper", 1980, 2),
+        Album(5, "Bossanova", 1990, 1),
+        Album(6, "Lover", 2019, 3),
+        Album(7, "Folklore", 2020, 3),
+        Album(8, "I Put a Spell on You", 1965, 4),
+        Album(9, "Baltimore", 1978, 4),
+        Album(10, "Here Comes the Sun", 1971, 4),
+        Album(11, "Fodder on My Wings", 1982, 4),
+        Album(12, "Ring Ring", 1973, 2),
+        Album(13, "test", 2000, 1)
+    ]
+
+
+# """
+# When we call PostRepository#delete
+# We remove a record from the database.
+# """
+
+
+# def test_delete_post(db_connection):
+#     db_connection.seed("seeds/social_network.sql")
+#     repository = PostRepository(db_connection)
+#     post = Post(4, "new Title", "new Content", 9, 2)
+#     assert repository.create(post) == None
+
+#     assert repository.delete(1) == None
+
+#     posts = repository.all()
+
+#     assert posts == [
+#         Post(2, "new live show", "we are on this week", 100, 2),
+#         Post(3, "el beers", "tour beers are good beers", 110000, 3),
+#         Post(4, "new Title", "new Content", 9, 2),
+#     ]
