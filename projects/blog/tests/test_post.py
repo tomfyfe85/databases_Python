07678fb45f5ef31aa19b537084b9a0cd3871b8b4
Tom Fyfe <tomfyfe85@gmail.com>
2023-12-02 00:14:24 +0000

    post.py - Post passes all tests in test_post.py

--- projects/blog/tests/test_post.py
+++ projects/blog/tests/test_post.py
@@ -1,30 +1,30 @@
 from lib.post import Post
 
-# """
-# Artist constructs with an id, name and genre
-# """
-# def test_artist_constructs():
-#     artist = Artist(1, "Test Artist", "Test Genre")
-#     assert artist.id == 1
-#     assert artist.name == "Test Artist"
-#     assert artist.genre == "Test Genre"
+"""
+Post constructs with an id, name and genre
+"""
+def test_Post_constructs():
+    post = Post(1, "Test Post", "Test Genre")
+    assert post.id == 1
+    assert post.title == "Test Post"
+    assert post.post_content == "Test Genre"
 
-# """
-# We can format artists to strings nicely
-# """
-# def test_artists_format_nicely():
-#     artist = Artist(1, "Test Artist", "Test Genre")
-#     assert str(artist) == "Artist(1, Test Artist, Test Genre)"
-#     # Try commenting out the `__repr__` method in lib/artist.py
-#     # And see what happens when you run this test again.
+"""
+We can format posts to strings nicely
+"""
+def test_artists_format_nicely():
+    post = Post(1, "Test title", "Test content")
+    assert str(post) == "Post(1, Test title, Test content)"
+    # Try commenting out the `__repr__` method in lib/post.py
+    # And see what happens when you run this test again.
 
-# """
-# We can compare two identical artists
-# And have them be equal
-# """
-# def test_artists_are_equal():
-#     artist1 = Artist(1, "Test Artist", "Test Genre")
-#     artist2 = Artist(1, "Test Artist", "Test Genre")
-#     assert artist1 == artist2
+"""
+We can compare two identical posts
+And have them be equal
+"""
+def test_artists_are_equal():
+    post1 = Post(1, "Test post", "Test content")
+    post2 = Post(1, "Test post", "Test content")
+    assert post1 == post2
     # Try commenting out the `__eq__` method in lib/artist.py
     # And see what happens when you run this test again.
